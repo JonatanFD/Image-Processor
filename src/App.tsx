13762e7483e2d1ae4d6c1fb911eb20f343312f37
Jonatan <acunacorahuajon@gmail.com>
2024-04-28 13:52:21 -0500

    adicionales

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Aside from "./components/Aside";
 import Histogram from "./components/Histogram";
 import BackIcon from "./components/icons/BackIcon";
+import GithubIcon from "./components/icons/GithubIcon";
 import { useImage } from "./hooks/useImage";
 
 function App() {
@@ -15,15 +16,17 @@ function App() {
 
             {url && (
                 <div className="flex items-center flex-col p-4 bg-[#26282C] rounded-xl gap-4 border-[#424549] border shadow-xl xl:flex-row relative">
-                    <Histogram/>
+                    <Histogram />
 
-                    <button className=" -top-12 right-0 bg-[#05A7A8] text-sm px-3 py-2 rounded-xl xl:absolute flex items-center gap-3 hover:shadow-2xl hover:shadow-[#05a8a891] transition-all duration-300"
-                    onClick={() => setUrl("")}
-                    > 
-                
-                    <BackIcon />
-                    
-                    Ingresar otra imagen</button>
+                    <div className="-top-12 right-0  xl:absolute flex items-center gap-3">
+
+                        <GithubIcon />
+
+                        <button onClick={() => setUrl("")} className="bg-[#05A7A8] flex items-center gap-3 hover:shadow-2xl  text-sm px-3 py-2 rounded-xl hover:shadow-[#05a8a891] transition-all duration-300">
+                            <BackIcon />
+                            Ingresar otra imagen
+                        </button>
+                    </div>
                 </div>
             )}
         </section>
